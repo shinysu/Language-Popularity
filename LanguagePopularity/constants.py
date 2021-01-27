13c1becb0698d5ceadd3c4d3f37ecded6848fc4f
Shiny S U <shiny.suresh@gmail.com>
2021-01-28 01:33:40 +0530

    Delete LanguagePopularity directory

--- LanguagePopularity/constants.py
+++
@@ -1,4 +0,0 @@
-projects_info_file = "project_info1.csv"
-modules_file = 'modules1.csv'
-github_api_request_url = 'https://api.github.com/search/repositories?q=language:python+archived:false&sort=stars&' \
-                         'order=desc&per_page=100&page=1'
--- LanguagePopularity/credentials.py
+++
@@ -1,1 +0,0 @@
-GITHUB_TOKEN_ID = ""  # Generate your Personal Access Token from GitHub and use it here
